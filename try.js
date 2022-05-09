cb2cbc887e85b54a76c8a2b2339a037bf6d4ff9f
SimonKovacsp22 <postcardsent@gmail.com>
2022-05-09 22:15:36 +0200

    Update

--- try.js
+++ try.js
@@ -1,9 +1,9 @@
-   
+console.log('5')
     function findUnique(array) {
         let count = 0
         let uniqueness = 0
         for (let index = 0; index < array.length; index++) {
-            for (let j= 0; j < array.length; index++) {
+            for (let j= 0; j < array.length; j++) {
                 if(array[index] === array[j])
                 uniqueness++;
                 
@@ -17,6 +17,6 @@
         console.log(count)
     }
 
-    findUnique([1,3,5,4,1,2,5,6])
+    findUnique([1,3,5,4,1,2,5,6,12,333,444])
     console.log('5')
     
